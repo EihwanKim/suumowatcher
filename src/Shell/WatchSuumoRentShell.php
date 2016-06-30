26c47bed1adf7362475492a41447e63ba40da25b
EihwanKim <cloz2me@gmail.com>
2016-06-30 23:42:54 +0900

    クローラを共通クラスに変更 src\Util

--- src/Shell/WatchSuumoRentShell.php
+++ src/Shell/WatchSuumoRentShell.php
@@ -9,95 +9,12 @@
 namespace App\Shell;
 
 use Cake\Console\Shell;
-use Goutte\Client;
-use Cake\ORM\TableRegistry;
+use App\Util\WatchSuumoRentUtil;
 
 class WatchSuumoRentShell extends Shell
 {
-
-    const SLEEP_SECONDS = 2;
-
     public function main() {
-
-        $pageUrl = "http://suumo.jp/jj/chintai/ichiran/FR301FC001/?ar=030&bs=040&ra=008&cb=0.0&ct=9999999&et=5&cn=9999999&mb=80&mt=9999999&shkr1=03&shkr2=03&shkr3=03&shkr4=03&fw2=&ek=076076150&rn=0760";
-
-        $client = new Client();
-        $crawler = $client->request('GET', $pageUrl);
-
-        //property_unit-content クラスリストを取得
-        $crawler->filter('.cassetteitem_other a')->each(function ($node) {
-
-            $text = trim($node->text());
-            $href = trim($node->attr('href'));
-
-            if ($text == '詳細を見る') {
-
-                $record = $this->detail($href);
-                $rentsTable = TableRegistry::get('Rents');
-                $rent = $rentsTable->newEntity();
-
-                $rent->url = $record['url'];
-                $rent->title = $record['title'];
-                $rent->price = $record['price'];
-                $rent->kanri_charge = $record['kanri_charge'];
-                $rent->sikikin = $record['sikikin'];
-                $rent->reikin = $record['reikin'];
-                $rent->place = $record['place'];
-                $rent->access = $record['access'];
-                $rent->width = $record['width'];
-                $rent->room_type = $record['room_type'];
-                $rent->floor = $record['floor'];
-                $rent->build_date = $record['build_date'];
-                $rent->etc = $record['etc'];
-                $rent->created = $record['created'];
-                $rentsTable->save($rent);
-
-            }
-        });
-    }
-
-    private function detail($pageUrl = '') {
-
-        if (!$pageUrl) {
-            return;
-        }
-
-        sleep (self::SLEEP_SECONDS);
-
-        $client = new Client();
-        $crawler = $client->request('GET', $pageUrl);
-
-        $record = array();
-        $record['url'] = $pageUrl;
-        $record['title'] = $crawler->filter('h1')->text();
-        $detail_str = preg_replace('/\s+/', '|', trim($crawler->filter('.detailinfo')->text()));
-        $record['etc'] = $detail_str;
-        $detail_array = explode('|', $detail_str);
-        $record['price'] = str_replace('万円', '', $detail_array[0]);
-        $record['kanri_charge'] = str_replace('円', '', $detail_array[2]) / 10000;
-        $record['sikikin'] = str_replace('万円', '', $detail_array[4]);
-        $record['reikin'] = str_replace('万円', '', $detail_array[6]);
-        $record['place'] = $detail_array[16];
-        $access = str_replace('[乗り換え案内]', '', trim($crawler->filter('.detailnote-value-list')->text()));
-        $record['access'] = str_replace(array("\r\n", "\r", "\n"), '', $access);
-        $record['width'] = $detail_array[12];
-        $record['room_type'] = $detail_array[11];
-//        $record['build_date'] = $detail_array[15];
-        $gaiyo_str = $crawler->filter('.data_table')->text();
-        $gaiyo_str = preg_replace('/\s+/', '|', trim($gaiyo_str));
-        $gaiyo_array = explode('|', $gaiyo_str);
-        for ($i = 0 ; $i < count($gaiyo_array); $i++) {
-            if ($gaiyo_array[$i] == '階建') {
-                $record['floor'] = $gaiyo_array[$i + 1];
-                continue;
-            }
-            if ($gaiyo_array[$i] == '築年月') {
-                $record['build_date'] = $gaiyo_array[$i + 1];
-                continue;
-            }
-
-        }
-        $record['created'] = date('Y-m-d');
-        return $record;
+        $utilRent = new WatchSuumoRentUtil();
+        $utilRent->exec();
     }
 }
