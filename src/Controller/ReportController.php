7783155e92614b4b004b696c86dae5d735f9af7f
EihwanKim <cloz2me@gmail.com>
2016-07-07 00:10:00 +0900

    report controller 作成

---
+++ src/Controller/ReportController.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eihwan
+ * Date: 7/3/16
+ * Time: 11:54
+ */
+
+namespace App\Controller;
+
+use Cake\ORM\TableRegistry;
+
+class ReportController extends AppController
+{
+    public function rent() {
+
+        $rents = TableRegistry::get('Rents');
+        $query = $rents->find()->orderAsc('url')->orderAsc('created');
+
+        if ($this->request->is('post')) {
+
+            if ($this->request->data['from']) {
+                $query->where(['created >=' => $this->request->data['from']]);
+            }
+
+            if ($this->request->data['to']) {
+                $query->where(['created >=' => $this->request->data['from']]);
+            }
+        }
+
+        $records = $query->toArray();
+
+        $graphData = [];
+        $data = [];
+        foreach($records as $key => $value) {
+
+            array_push($data, [$value->created->getTimestamp() * 1000, floatval($value->price)]);
+
+            if ($value->url != $records[$key + 1]['url']) {
+                array_push($graphData, [
+                    'name' => $records[$key]['width'] . ' | ' .
+                            $records[$key]['room_type']  . ' | ' .
+                            $records[$key]['floor'],
+                    'data' =>$data
+                ]);
+
+                $data = [];
+            }
+
+        }
+
+        $graphJson = json_encode($graphData);
+        $this->set(compact('graphJson'));
+
+    }
+
+    public function sell() {
+
+        $rents = TableRegistry::get('Sells');
+        $query = $rents->find()->orderAsc('url')->orderAsc('created');
+
+        if ($this->request->is('post')) {
+
+            if ($this->request->data['from']) {
+                $query->where(['created >=' => $this->request->data['from']]);
+            }
+
+            if ($this->request->data['to']) {
+                $query->where(['created >=' => $this->request->data['from']]);
+            }
+        }
+
+        $records = $query->toArray();
+
+        $graphData = [];
+        $data = [];
+        foreach($records as $key => $value) {
+
+            array_push($data, [$value->created->getTimestamp() * 1000, floatval($value->price)]);
+
+            if ($value->url != $records[$key + 1]['url']) {
+                array_push($graphData, [
+                    'name' => $records[$key]['width'] . ' | ' .
+                        $records[$key]['room_type']  . ' | ' .
+                        $records[$key]['floor'],
+                    'data' =>$data
+                ]);
+
+                $data = [];
+            }
+
+        }
+
+        $graphJson = json_encode($graphData);
+        $this->set(compact('graphJson'));
+
+    }
+
+}
